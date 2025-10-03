6df98bdb468f4b53e8d74e7bbe4a7ba089413d0c
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2025-10-03 19:01:45 +0530

    jQuery demo file added

---
+++ jQuery/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#btn1').click(function() {
+        alert('Button 1 clicked!');
+    }); 
+    $('#btn2').click(function() {
+        alert('Button 2 clicked!');
+    });
+});
