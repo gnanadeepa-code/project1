9dadbb1cccfe814c0e10e587870d0d7c0d9d7cb4
gnanadeepa-code <gnanadeepa.perumal@gmail.com>
2025-10-04 20:08:58 +0530

    js changes

--- jQuery/script.js
+++ jQuery/script.js
@@ -2,7 +2,4 @@ $(document).ready(function() {
     $('#btn1').click(function() {
         alert('Button 1 clicked!');
     }); 
-    $('#btn2').click(function() {
-        alert('Button 2 clicked!');
-    });
 });
